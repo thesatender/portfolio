35fadcfe209499561042504f990674131f313978
Satender sharma <satender80010336@gmail.com>
2023-06-29 00:36:19 +0530

    projects updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import tindog from "../../Assets/Projects/tindog.png";
+import nlpMcqGeneration from "../../Assets/Projects/nlpMcqGeneration.png";
+import keeper from "../../Assets/Projects/keeper.png";
 
 function Projects() {
   return (
@@ -23,68 +21,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={nlpMcqGeneration}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/thesatender/Chatify"
-              demoLink="https://chatify-501.web.app/"
+              title="MCQ Generation using NLP"
+              description="MCQ Generation using NLP is a project that utilizes Natural Language Processing techniques to generate multiple-choice questions. It integrates concepts like summarization, keyword extraction, distractor generation, and word sense disambiguation. This project aims to assist educators by providing a tool to quickly generate relevant and meaningful questions from text."
+              ghLink="https://github.com/thesatender/mcq-generation-nlp"
+              // demoLink="https://satender.com/"    <-- Update later
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={keeper}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              title="Keeper"
+              description="Discover a simple and intuitive note-taking solution built with React. Our Google Keep clone allows you to effortlessly jot down and organize your ideas, tasks, and reminders. Enjoy a clean and user-friendly interface and an easy way to stay productive and organized. Start taking better notes today!"
               ghLink="https://github.com/satender/Bits-0f-C0de"
-              demoLink="https://blogs.satender.tech/"
+              demoLink="https://keeper-thesatender.vercel.app/"   
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={tindog}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/satender/Editor.io"
-              demoLink="https://editor.satender-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/satender/Plant_AI"
-              demoLink="https://plant99-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/satender/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/satender/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="TinDog"
+              description="The TinDog App is a fun and fictional website inspired by the popular dating app, Tinder. Built using HTML and Bootstrap CSS, this unique website is designed exclusively for dogs. Dog owners can enjoy a playful interface, 'swiping' through profiles to find potential furry companions and make connections in the doggy world. Get ready for some Woof-tastic fun!"
+              ghLink="https://github.com/thesatender/TinDog"
+              demoLink="https://tindog-thesatender.vercel.app/"  
             />
           </Col>
         </Row>
